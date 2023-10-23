fe438de36d0a6eb8380ddf91706f2094c280feb7
Nathan Souza <naathanmarinho11@gmail.com>
2023-10-23 17:48:00 -0300

    initial commit for database configuration

---
+++ parameters.py
@@ -0,0 +1,12 @@
+# configuration connection to database
+
+bd_config = {
+    "host": "your_host",
+    "user": "your_user",
+    "password": "your_password",
+    "database": "your_database"
+}
+
+# folder where the files to be read are located
+
+folder = "path/to/your/folder"

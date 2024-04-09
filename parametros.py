970ad952847cba2e7faef1b3bc16ce15b334e18c
Nathan Souza <naathanmarinho11@gmail.com>
2024-04-09 18:22:47 -0300

    altera nome arquivo

---
+++ parametros.py
@@ -0,0 +1,13 @@
+# Configuração de conexão com banco de dados
+bd_config = {
+    "host": "SEU_HOST",
+    "user": "SEU_USUÁRIO",
+    "password": "SUA_SENHA",
+    "database": "NOME_DATABASE"
+}
+
+# Pasta onde estão localizados os arquivos a serem lidos
+pasta = "path/to/your/folder"
+
+# Lista de extensões usadas para filtrar tipos de arquivos
+extensoes = ['.csv','.xls', '.xlsx']
